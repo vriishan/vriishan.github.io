1de72852996c5cd53ae71df998e6c581cc709c0c
Vrushab Shandilya <cooperalan97@gmail.com>
2024-05-20 14:46:01 -0700

    fix main page elements' css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,13 @@ import { useState, useEffect } from "react";
 
 const Navbar = () => {
 
+    const hoverVariant = {
+        "hover": {
+            scale: 1.2,
+            transition: { duration: 0.2 },
+        }
+      }
+
     // const ref = useRef();
     const [hidden, setHidden] = useState(true);
 
@@ -34,21 +41,30 @@ const Navbar = () => {
             transition={{ duration: 0.5, ease: "easeInOut"}}
         >
             <div className="wrapper">
-                {/* <motion.span
-                    className="motion-span"
-                    initial={{opacity: 0, scale: 0.5}} 
-                    animate={{opacity: 1, scale: 1}}
-                    transition={{duration: 1}}
-                >
-                    Vrushab Shandilya
-                </motion.span> */}
-                <div className="links">
-                    <a href="#">Home</a>
-                    <a href="#">About</a>
-                    <a href="#">Skills</a>
-                    <a href="#">Experience</a>
-                    <a href="#">Projects</a>
+                <div className="navbarLeft">
+                    <motion.span
+                        className="motion-span"
+                        initial={{opacity: 0, scale: 0.5}} 
+                        animate={{opacity: 1, scale: 1}}
+                        transition={{duration: 1}}
+                    >
+                        &lt;Vrushab/&gt;
+                    </motion.span>
                 </div>
+                <div className="navbarMiddle">
+                    <div className="links">
+                        <motion.a href="#" variants={hoverVariant} whileHover="hover">Home</motion.a>
+                        <motion.a href="#" variants={hoverVariant} whileHover="hover">About</motion.a>
+                        <motion.a href="#" variants={hoverVariant} whileHover="hover">Skills</motion.a>
+                        <motion.a href="#" variants={hoverVariant} whileHover="hover">Experience</motion.a>
+                        <motion.a href="#" variants={hoverVariant} whileHover="hover">Projects</motion.a>
+                    </div>
+                </div>
+                <div className="navbarRight">
+
+                </div>
+                
+                
                 {/* <div className="social">
                     <a href="#"><img src="./linkedin.png" alt="" /></a>
                     <a href="#"><img src="./instagram.png" alt="" /></a>
